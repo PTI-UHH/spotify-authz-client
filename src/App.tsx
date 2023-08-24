48407f322e964dc692d400780039fcc5c744bd46
texhnolyze <joegriepen@gmail.com>
2023-08-24 13:42:52 +0200

    feat(userId): save after authorization with spotify
    to localstorage and send it to the polling server as part of the url
    path

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,51 @@
+import { useState } from 'react';
 import { UserIdForm } from './components/userIdForm';
 import { Scopes, SpotifyApi } from '@spotify/web-api-ts-sdk';
 import './App.css'
 
+const LOCAL_STORAGE_USER_ID_KEY = 'PTI_USER_ID'
+
 function App() {
-  const userId = null;
-  return userId ? userIdForm() : "";
-}
-
-function userIdForm() {
-  return ( 
-    <UserIdForm onSubmit={(userId: string) => {
-      userId = userId; 
-      authorizeWithSpotify(userId);
-    }} />
+  const [userId, setUserId] = useState(
+    JSON.parse(localStorage.getItem(LOCAL_STORAGE_USER_ID_KEY)) || null
   )
-}
 
-function authorizeWithSpotify(userId: string) {
-  SpotifyApi.performUserAuthorization(
-    import.meta.env.VITE_SPOTIFY_CLIENT_ID, 
-    import.meta.env.VITE_REDIRECT_TARGET, 
-    Scopes.userPlaybackRead, 
-    import.meta.env.VITE_SERVER_USER_TOKEN_URL, 
-  );
+  return userId
+    ? alreadyAuthorizedMessage()
+    : userIdForm();
+
+  function updateUserId(userId: string) {
+    localStorage.setItem(LOCAL_STORAGE_USER_ID_KEY, JSON.stringify(userId));
+    setUserId(userId);
+  }
+
+  function alreadyAuthorizedMessage() {
+    return (
+      <h1>
+        User {userId}, you have already authorized collection of Spotify data.
+        Thank you for your participation!
+      </h1>
+    )
+  }
+
+  function userIdForm() {
+    return (
+      <UserIdForm onSubmit={authorizeWithSpotify} />
+    )
+  }
+
+  async function authorizeWithSpotify(userId: string) {
+    await SpotifyApi.performUserAuthorization(
+      import.meta.env.VITE_SPOTIFY_CLIENT_ID,
+      import.meta.env.VITE_AUTH_REDIRECT_TARGET,
+      Scopes.userPlaybackRead,
+      `${import.meta.env.VITE_POLLING_SERVER_BASE_URL}/${userId}/auth`,
+    );
+
+    setTimeout(() => {
+      updateUserId(userId);
+    }, 1000);
+  }
 }
 
 export default App;
