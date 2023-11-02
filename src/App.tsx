f8a4d3c1e383fd5ec57eb5106cda7aaefa80da59
texhnolyze <joegriepen@gmail.com>
2023-11-02 16:12:00 +0100

    feat(UserForm): use form to submit id/email/accessToken

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
 import { Scopes, SpotifyApi } from "@spotify/web-api-ts-sdk";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./App.css";
-import {
-  usePostSpotifyAccessToken,
-  useSpotifyApi,
-} from "./hooks/useSpotify";
+import { usePostSpotifyAccessToken, useSpotifyApi } from "./hooks/useSpotify";
 import { User, UserState } from "./types";
+import { UserForm, UserFormData } from "./components/UserForm";
 
 const LOCAL_STORAGE_USER_KEY = "PTI_USER";
-
-function getUserIdFromUrlPath() {
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
-  return urlParams.get("userId");
-}
 
 function getFromLocalStorage(key: string) {
   return JSON.parse(localStorage.getItem(key) || "{}");
@@ -24,75 +16,72 @@ function saveToLocalStorage(key: string, value: unknown) {
 }
 
 function App() {
-  const initialUserState = {
-    state: UserState.LOGGED_OUT,
-    ...getFromLocalStorage(LOCAL_STORAGE_USER_KEY),
-  };
+  const initialUserState = { ...getFromLocalStorage(LOCAL_STORAGE_USER_KEY) };
   const [user, setUser] = useState<User>(initialUserState);
-  const pathUserId = getUserIdFromUrlPath();
-  const newUserIdWasPassed = pathUserId && user.id !== pathUserId;
+  const status = useRef<{ success: boolean; message: string }>({
+    success: false,
+    message: "",
+  });
+
+  const spotifyApi = useSpotifyApi(
+    import.meta.env.VITE_SPOTIFY_CLIENT_ID,
+    import.meta.env.VITE_AUTH_REDIRECT_TARGET,
+    [...Scopes.userPlaybackRead],
+    user
+  );
 
   function updateUser(userData: Partial<User>) {
     saveToLocalStorage(LOCAL_STORAGE_USER_KEY, { ...user, ...userData });
     setUser((user: User) => ({ ...user, ...userData }));
   }
 
-  function handleUserStateTransitions(spotifyApi: SpotifyApi | null) {
-    if (spotifyApi) {
-      switch (user.state) {
-        case UserState.LOGGED_OUT:
-          updateUser({ state: UserState.LOGGED_IN });
-          break;
-        case UserState.CHANGED:
-          spotifyApi.logOut();
-          updateUser({ state: UserState.LOGGED_OUT });
-          break;
-        case UserState.LOGGED_IN:
-          break;
+  function resetUser() {
+    saveToLocalStorage(LOCAL_STORAGE_USER_KEY, {});
+    setUser(() => ({}));
+  }
+
+  function handleFormSubmit(data: UserFormData) {
+    updateUser({ ...data });
+  }
+
+  const response = usePostSpotifyAccessToken(spotifyApi, user);
+  useEffect(() => {
+    (async () => {
+
+    if (response) {
+      status.current.success = response.ok;
+
+      if (response.ok) {
+        resetUser();
+        const json = await response.json();
+        status.current.message = `POST to ${response.url} success: ${response.status}\n ${json}`;
+      } else {
+        const text = await response.text()
+        status.current.message = `POST to ${response.url} failed: ${response.status}\n ${text}`;
       }
+
+      console.log(status)
+      console.log(response)
+    } else {
+      status.current.success = false;
+      status.current.message = "";
     }
-  }
+    })()
+  }, [response, spotifyApi, status]);
 
-  function renderStatusMessage() {
-    if (!user.id) {
-      return <h1>Missing "userId" query parameter in URL!</h1>;
-    }
+  const messageColorClass =
+    status.current.success && status.current.message
+      ? "text-green-500"
+      : "text-red-500";
 
-    let statusMessage = null;
-    switch (user.state) {
-      case UserState.LOGGED_OUT:
-        statusMessage = "Not yet authorized!";
-        break;
-      case UserState.CHANGED:
-        statusMessage = "Changed user, reauthorizing!";
-        break;
-      case UserState.LOGGED_IN:
-        statusMessage = "Already authorized!";
-        break;
-    }
-
-    return (
-      <h1>
-        User with id "{user.id}", has status {user.state}.
-        <br />
-        {statusMessage}
-      </h1>
-    );
-  }
-
-  const spotifyApi = useSpotifyApi(
-    import.meta.env.VITE_SPOTIFY_CLIENT_ID,
-    import.meta.env.VITE_AUTH_REDIRECT_TARGET,
-    [...Scopes.userPlaybackRead],
-    user.state
+  return (
+    <div className="w-full flex flex-col flex-auto justify-center items-center bg-gray-200">
+      <UserForm onSubmit={handleFormSubmit}></UserForm>
+      <p className={`text-xl font-bold ${messageColorClass}`}>
+        {status.current.message}
+      </p>
+    </div>
   );
-  usePostSpotifyAccessToken(spotifyApi, user);
-
-  if (newUserIdWasPassed) {
-    updateUser({ id: pathUserId, state: UserState.CHANGED });
-  }
-  handleUserStateTransitions(spotifyApi);
-  return renderStatusMessage();
 }
 
 export default App;
--- src/hooks/useSpotify.ts
+++ src/hooks/useSpotify.ts
@@ -2,42 +2,61 @@ import { SpotifyApi } from "@spotify/web-api-ts-sdk";
 import { useEffect, useRef, useState } from "react";
 import { User, UserState } from "../types";
 
+const isUserValid = (user: User) => user?.id && user?.email;
+
 export function useSpotifyApi(
   clientId: string,
   redirectUrl: string,
   scopes: string[],
-  userState: UserState
+  user: User
 ) {
+  console.log("useSpotifyApi called");
   const [spotifyApi, setSpotifyApi] = useState<SpotifyApi | null>(null);
   const { current: activeScopes } = useRef(scopes);
 
   useEffect(() => {
     (async () => {
-      const api = SpotifyApi.withUserAuthorization(
-        clientId,
-        redirectUrl,
-        activeScopes
-      );
+      function resetSpotifyApi() {
+        spotifyApi?.logOut();
+        setSpotifyApi(() => null);
+        return;
+      }
 
-      try {
-        const { authenticated } = await api.authenticate();
+      async function setupSpotifyApi() {
+        const api = SpotifyApi.withUserAuthorization(
+          clientId,
+          redirectUrl,
+          activeScopes
+        );
 
-        if (authenticated) {
-          setSpotifyApi(() => api);
-        }
-      } catch (e: Error | unknown) {
-        const error = e as Error;
-        if (error?.message?.includes("No verifier found in cache")) {
-          console.error(
-            "If you are seeing this error in a React Development Environment it's because React calls useEffect twice. Using the Spotify SDK performs a token exchange that is only valid once, so React re-rendering this component will result in a second, failed authentication. This will not impact your production applications (or anything running outside of Strict Mode - which is designed for debugging components).",
-            error
-          );
-        } else {
-          console.error(e);
+        try {
+          const { authenticated } = await api.authenticate();
+
+          if (authenticated) {
+            setSpotifyApi(() => api);
+          }
+        } catch (e: Error | unknown) {
+          const error = e as Error;
+          if (error?.message?.includes("No verifier found in cache")) {
+            console.error(
+              "If you are seeing this error in a React Development Environment it's because React calls useEffect twice. Using the Spotify SDK performs a token exchange that is only valid once, so React re-rendering this component will result in a second, failed authentication. This will not impact your production applications (or anything running outside of Strict Mode - which is designed for debugging components).",
+              error
+            );
+          } else {
+            console.error(e);
+          }
         }
       }
+
+      if (!isUserValid(user)) {
+        resetSpotifyApi();
+      }
+
+      if (!spotifyApi && isUserValid(user)) {
+        await setupSpotifyApi();
+      }
     })();
-  }, [clientId, redirectUrl, activeScopes, userState]);
+  }, [clientId, redirectUrl, activeScopes, user, spotifyApi]);
 
   return spotifyApi;
 }
@@ -46,23 +65,31 @@ export function usePostSpotifyAccessToken(
   spotifyApi: SpotifyApi | null,
   user: User
 ) {
+  const [response, setResponse] = useState<Response | null>(null);
+
   useEffect(() => {
     (async () => {
       const tokenPostUrl = `${
         import.meta.env.VITE_POLLING_SERVER_BASE_URL
       }/user/${user.id}/auth`;
 
-      if (spotifyApi && user.state === UserState.LOGGED_IN) {
+      if (spotifyApi && isUserValid(user)) {
         const accessToken = await spotifyApi.getAccessToken();
 
-        await fetch(tokenPostUrl, {
+        const result = await fetch(tokenPostUrl, {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify(accessToken),
+          body: JSON.stringify({ email: user.email, ...accessToken }),
         });
+
+        setResponse(() => result);
+      } else {
+        setResponse(() => null);
       }
     })();
   }, [spotifyApi, user]);
+
+  return response;
 }
